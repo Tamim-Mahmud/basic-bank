5a67fd3baa2fdda9679c28c6d0dfb05573bbecb3
TamimMahmud <tmahmud.jnubd@gmail.com>
2023-06-13 18:27:25 +0600

    password field enter functionlaity added

--- js/login.js
+++ js/login.js
@@ -1,13 +1,19 @@
-document.getElementById('login-button').addEventListener('click',function(){
-    const userTypedEmail=document.getElementById('email-field').value;
-    const userTypedPassword=document.getElementById('password-field').value;
+document.getElementById('login-button').addEventListener('click', function () {
+    const userTypedEmail = document.getElementById('email-field').value;
+    const userTypedPassword = document.getElementById('password-field').value;
 
-    if(userTypedEmail=='tamim@gmail.com' && userTypedPassword=='admin'){
-        window.location.href= 'bank.html';
+    if (userTypedEmail == 'tamim@gmail.com' && userTypedPassword == 'admin') {
+        window.location.href = 'bank.html';
     }
-    else{
+    else {
         alert('Invalid Email or Password');
     }
-    
 
-})
+
+});
+document.getElementById('password-field').addEventListener('keypress', function (event) {
+    if (event.key == "Enter") {
+        document.getElementById("login-button").click();
+    }
+});
+
